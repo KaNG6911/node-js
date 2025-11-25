4386a96aefc5e1f6546a2fc886bfc6743ffa6f53
25LP5951 25LP5951 <gna.k6911@gmail.com>
2025-11-25 12:24:44 +0800

    djfhdjhfd

---
+++ js/dom2.js/index.js
@@ -0,0 +1,11 @@
+const hourTime = document.getElementById("hour");
+const minTime = document.getElementById("min");
+const secTime = document.getElementById("sec");
+const startBtn = document.getElementById("butt1");
+const stopBtn = document.getElementById("butt2");
+const resetBtn = document.getElementById("butt3");
+
+startBtn.onclick = hourFunc;
+function hourFunc() {
+  const secTime = "99";
+}
